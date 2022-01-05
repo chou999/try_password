0f82f1416326443b727bbf946b61f4a8c6f51178
CCChou <83779490+chou999@users.noreply.github.com>
2022-01-06 00:55:17 +0800

    Add files via upload
    final version

--- try_password.py
+++ try_password.py
@@ -4,6 +4,11 @@ while i > 0:
     i = i - 1
     if password == 'a123456':
     	print('登入成功！')
-    	break # 逃出迴圈
+    	break # 逃出回圈
     else:
-    	print('密碼錯誤！你還有', i,'次機會！')
+    	print('密碼錯誤！')
+    	if i > 0:
+    		print('你還有', i,'次機會！')
+    	else:
+    		print('你沒機會嘗試了！')
+
